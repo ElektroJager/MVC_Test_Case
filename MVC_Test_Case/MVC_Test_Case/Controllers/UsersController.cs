a0ba1f4d04559f4310176b1a70d06d9994ee0866
Erolcan Hasmaden <erolcanhasmaden53@gmail.com>
2021-09-25 18:33:06 +0300

    v 0 . 0 . 1

---
+++ MVC_Test_Case/MVC_Test_Case/Controllers/UsersController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Test_Case.Models;
+
+namespace MVC_Test_Case.Controllers
+{
+    public class UsersController : Controller
+    {
+        private UserDBContext db = new UserDBContext();
+
+        // GET: Users
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        // GET: Users/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(User user)
+        {
+            Session["Name"] = user.Name;
+            return View();
+        }
+
+        // GET: Users/Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        // GET: Users/Logout
+        public ActionResult Logout()
+        {
+            Session["Name"] = null;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(User user)
+        {
+    
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Login");
+            }
+
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MVC_Test_Case/MVC_Test_Case/Models/Users.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Test_Case.Models
+{
+    public class User
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public string Surname { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+
+    public class UserDBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
